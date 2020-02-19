e99bbafc300f25a21984cf315a91de5a17951b9e
tuj-aerish <61077150+tuj-aerish@users.noreply.github.com>
2020-02-19 11:27:43 +0800

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+#start inputs: Tuj
+
+from blog.models import Post, Category
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+#end inputs: Tuj
