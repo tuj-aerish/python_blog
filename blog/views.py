9083852d5c322b797af4ec3b62d7dc8151becd10
tuj-aerish <61077150+tuj-aerish@users.noreply.github.com>
2020-02-19 11:29:25 +0800

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+#start inputs: Tuj
+from blog.models import Post
+
+def blog_index(request):
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
+#end inputs: Tuj
